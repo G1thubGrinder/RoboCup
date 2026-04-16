158b97246d04d027dacff27bfe1bc66fb0dad41a
G1thubGrinder <peeburapha@gmail.com>
2026-04-17 01:10:43 +0700

    [fix] change shell script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Starting local server at http://localhost:8080"
+echo "Starting local server at http://localhost:8080/app/visualizer.html"
 cd "$(dirname "$0")"
 python3 -m http.server 8080
